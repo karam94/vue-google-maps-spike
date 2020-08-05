07a9deba8aa93352f1ebdec80fe05a3ffbf1b2aa
Karam Kabbara <contact@karam.io>
2020-08-05 17:53:18 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <TravelMap class="travel-map" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TravelMap from "./components/TravelMap";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    TravelMap,
+  },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.travel-map {
+  position: absolute;
+  height: 100%;
+  width: 100%;
 }
 </style>
---
+++ src/components/TravelMap.vue
@@ -0,0 +1,59 @@
+<template>
+  <GoogleMapLoader :mapConfig="mapConfig" apiKey="">
+    <template slot-scope="{ google, map }">
+      <GoogleMapMarker
+        v-for="marker in markers"
+        :key="marker.id"
+        :marker="marker"
+        :google="google"
+        :map="map"
+      />
+    </template>
+  </GoogleMapLoader>
+</template>
+
+<script>
+import GoogleMapLoader from "./GoogleMapLoader";
+import GoogleMapMarker from "./GoogleMapMarker";
+
+import { mapSettings } from "@/constants/mapSettings";
+
+export default {
+  components: {
+    GoogleMapLoader,
+    GoogleMapMarker,
+  },
+
+  data() {
+    return {
+      markers: [
+        {
+          id: "AND Kilburn Office",
+          position: { lat: -34.771940, lng: 138.618950 },
+        },
+        {
+          id: "Co-op One Angel Square",
+          position: { lat: 53.487690, lng: -2.237120 },
+        },
+        {
+          id: "AND Murray Office",
+          position: { lat: 53.7934982, lng: -1.5578858 },
+        }
+      ]
+    };
+  },
+
+  computed: {
+    mapConfig() {
+      return {
+        ...mapSettings,
+        center: this.mapCenter,
+      };
+    },
+
+    mapCenter() {
+      return this.markers[1].position;
+    },
+  },
+};
+</script>
